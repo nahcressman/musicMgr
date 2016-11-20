e3fdd983a03b838a03ed9422cd06389f35e154f4
Nick Hahn <nicko1061@gmail.com>
2016-11-19 16:39:43 -0800

    Getting email back from spotify, storing in session

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-var express = require("express")
-var Spotify = require("./lib/spotify")
+var express = require("express");
+var sessions = require("client-sessions");
+var Spotify = require("./lib/spotify");
 
 var app = express();
+
+app.use(sessions({
+	cookieName: 'musicMgr', // cookie name dictates the key name added to the request object 
+	secret: '', // should be a large unguessable string 
+	duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms 
+}));
+
+app.use(function(req,res,next) {
+	if(req.musicMgr.email) {
+		console.log("session: has repeat user " + req.musicMgr.email);
+	}
+	next();
+});
+
 app.get('/search', function(req, res) {
 	var queryText = req.query.q;
 	if(queryText) {
@@ -20,7 +35,18 @@ app.get('/loggedIn', function(req, res) {
 	}
 	var code = req.query.code;
 	var state = req.query.state;
-	Spotify.getToken(code, state);
+	Spotify.getToken(code, state, function (result, auth_token, refresh_token, expirationTime) {
+		//get the email address for the user
+		if(result) {
+			Spotify.getUserDetails(auth_token, function(details) {
+				if(details && details.email) {
+					console.log("got user email. it's " + details.email);
+					req.musicMgr.email = details.email;
+				}
+			});
+		}		
+	});
+	res.redirect('/');
 });
 
 app.use(express.static('public'));
